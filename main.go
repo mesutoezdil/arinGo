8332525a402930ea5ccce7d42379665a5e2d1086
mesutoezdil <mesudozdil@gmail.com>
2024-08-14 22:47:10 +0200

    Commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//var name string = "Alo"
+	name := "Alo"
+
+	//var age int = 40
+	age := 40
+
+	var isMarried bool = false
+
+	fmt.Println("Hi!", name, age, isMarried)
+}
