21cf53a544d6d6f2b1c1c85a840f5d82a6034cfb
Mesut Oezdil <114185853+mesutoezdil@users.noreply.github.com>
2024-08-18 22:33:52 +0200

    Update main.go

--- main.go
+++ main.go
@@ -64,3 +64,21 @@ func main() {
 ", people, probability)
 	}
 }
+
+			   import (
+    "crypto/rand"
+    // "encoding/base64"
+    // "encoding/hex"
+    "fmt"
+)
+
+func Key() string {
+    buf := make([]byte, 16)
+    _, err := rand.Read(buf)
+    if err != nil {
+        panic(err)  // out of randomness, should never happen
+    }
+    return fmt.Sprintf("%x", buf)
+    // or hex.EncodeToString(buf)
+    // or base64.StdEncoding.EncodeToString(buf)
+}
