01ba25588131e194f4852e21bc3057dbbe6e90d8
Mesut Oezdil <114185853+mesutoezdil@users.noreply.github.com>
2024-08-21 22:59:12 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
-import (
-    "crypto/rand"
-    // "encoding/base64"
-    // "encoding/hex"
-    "fmt"
+module github.com/bitfield/script
+
+go 1.18
+
+require (
+	github.com/google/go-cmp v0.5.9
+	github.com/itchyny/gojq v0.12.13
+	github.com/rogpeppe/go-internal v1.11.0
+	mvdan.cc/sh/v3 v3.7.0
 )
 
-func Key() string {
-    buf := make([]byte, 16)
-    _, err := rand.Read(buf)
-    if err != nil {
-        panic(err)  // out of randomness, should never happen
-    }
-    return fmt.Sprintf("%x", buf)
-    // or hex.EncodeToString(buf)
-    // or base64.StdEncoding.EncodeToString(buf)
-}
+require (
+	github.com/itchyny/timefmt-go v0.1.5 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/tools v0.11.0 // indirect
+)
