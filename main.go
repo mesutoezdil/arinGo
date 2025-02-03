d95bc08db60f235ad2427da8a063c392bb68eabe
Mesut Oezdil <114185853+mesutoezdil@users.noreply.github.com>
2025-02-03 23:06:26 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,54 @@
-module github.com/bitfield/script
+package main
 
-go 1.18
+import (
+	"fmt"
 
-require (
-	github.com/google/go-cmp v0.5.9
-	github.com/itchyny/gojq v0.12.13
-	github.com/rogpeppe/go-internal v1.11.0
-	mvdan.cc/sh/v3 v3.7.0
+	"github.com/gogf/gf/os/gfile"
+	"github.com/gogf/gf/os/glog"
 )
 
-require (
-	github.com/itchyny/timefmt-go v0.1.5 // indirect
-	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/tools v0.11.0 // indirect
-)
+func main() {
+	filePath := "example_file.txt"
+
+	// Check if the file exists
+	if gfile.Exists(filePath) {
+		glog.Info("File exists: ", filePath)
+	} else {
+		glog.Info("File does not exist, creating: ", filePath)
+		initialContent := "This file was created using the GoFrame gfile package.\n"
+		if err := gfile.PutContents(filePath, initialContent); err != nil {
+			glog.Error("Error creating file: ", err)
+			return
+		}
+	}
+
+	// Append content to the file
+	appendContent := "Appending a line: Performing file operations in Golang.\n"
+	if err := gfile.PutContentsAppend(filePath, appendContent); err != nil {
+		glog.Error("Error appending to file: ", err)
+		return
+	}
+
+	// Read file content
+	content, err := gfile.GetContents(filePath)
+	if err != nil {
+		glog.Error("Error reading file: ", err)
+		return
+	}
+	fmt.Println("File content:")
+	fmt.Println(content)
+
+	// Get file size in bytes
+	size, err := gfile.Size(filePath)
+	if err != nil {
+		glog.Error("Error getting file size: ", err)
+		return
+	}
+	fmt.Printf("File size: %d bytes\n", size)
+
+	// Retrieve directory and base name from the file path
+	directory := gfile.Dir(filePath)
+	baseName := gfile.Basename(filePath)
+	fmt.Println("File directory:", directory)
+	fmt.Println("File name:", baseName)
+}
